a529f3720728fc34c13ed78e1f3b6eb38eb75d8c
JotaOG <85458044+JotaOG@users.noreply.github.com>
2021-10-28 17:57:24 -0400

    Add files via upload

---
+++ PersonCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PersonCard extends Component {
+    render() {
+        return(
+            <>
+                <h1>{this.props.lastName}, {this.props.firstName}</h1>
+                <p>Age: {this.props.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+            </>
+        );
+    }
+}
+export default PersonCard;
